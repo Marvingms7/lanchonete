4a5fde9182126e6ef8a3f6d13259a9f564d6c1a9
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-02-25 11:18:24 -0300

    Cores
    Alterando cores do thema

--- mark_delivery_app/lib/app/core/ui/styles/colors_app.dart
+++ mark_delivery_app/lib/app/core/ui/styles/colors_app.dart
@@ -1,11 +1,21 @@
-class ColorsApp{
-  static ColorsApp? _instance;
+import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+class ColorsApp {
+  static ColorsApp? _instance;
 
   ColorsApp._();
 
-  static ColorsApp get i{
+  static ColorsApp get i {
     _instance ??= ColorsApp._();
     return _instance!;
   }
+
+  Color get primary => const Color(0xFF007D21);
+  Color get secundary => const Color(0xFFF88B0C);
+}
+
+extension ColorsAppExcetions on BuildContext {
+  ColorsApp get colors => ColorsApp.i;
 }
--- mark_delivery_app/lib/app/pages/splash/splash_page.dart
+++ mark_delivery_app/lib/app/pages/splash/splash_page.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
+    return Theme(
+      data: ThemeData(scaffoldBackgroundColor: Colors.white),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Splash'),
+        ),
+        body: Container(),
       ),
-      body: Container(),
     );
   }
 }
