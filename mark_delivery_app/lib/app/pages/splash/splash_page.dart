3c5ec30c1143bed9d08f547e69c2a56529938fe8
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-02-25 11:27:21 -0300

    Update splash_page.dart

--- mark_delivery_app/lib/app/pages/splash/splash_page.dart
+++ mark_delivery_app/lib/app/pages/splash/splash_page.dart
@@ -8,7 +8,12 @@ class SplashPage extends StatelessWidget {
     return Theme(
       data: ThemeData(
         scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
       ),
       child: Scaffold(
         appBar: AppBar(
