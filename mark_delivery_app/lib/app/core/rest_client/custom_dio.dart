08232e1bfc052cb854df288f5c43402ef574c03e
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-03-08 17:06:16 -0300

    Update custom_dio.dart

--- mark_delivery_app/lib/app/core/rest_client/custom_dio.dart
+++ mark_delivery_app/lib/app/core/rest_client/custom_dio.dart
@@ -9,6 +9,7 @@ class CustomDio extends DioForNative {
             baseUrl: Env.i['backend_base_url'] ?? '',
             connectTimeout: const Duration(milliseconds: 5000),
             receiveTimeout: const Duration(milliseconds: 60000))) {
-    interceptors.add(LogInterceptor());
+    interceptors.add(
+        LogInterceptor(request: true, requestBody: true, requestHeader: true));
   }
 }
